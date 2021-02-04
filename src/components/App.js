2b8a45d9b650a5de528c66a051a203bc2b778862
jkang91 <jkang34@gmail.com>
2021-02-04 03:41:03 -0500

    hooks state events mini challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useState} from 'react';
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +9,25 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [category, setCategory] = useState("All")
+  const [tasks, setTasks] = useState(TASKS)
+
+  const visibleTasks = tasks.filter(task => category === "All" || task.category === category);
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask])
+  }
+
+  function handleDeleteTask(deletedTaskText) {
+    setTasks(tasks.filter(task => task.text !== deletedTaskText))
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCategory={category} onSelectedCategory={setCategory}/>
+      <NewTaskForm categories={CATEGORIES.filter((cat) => cat !== "All")} onTaskFormSubmit={handleAddTask}/>
+      <TaskList onDeleteTask={handleDeleteTask} tasks={visibleTasks}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSelectedCategory}) {
+  const categoryButtons = categories.map((category => {
+    const className = category === selectedCategory ? "selected" : null;
+    return (
+      <button 
+      key={category}
+      onClick={() => onSelectedCategory(category)}
+      className={className}
+      >
+        {category}
+      </button>
+    )
+  }))
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
